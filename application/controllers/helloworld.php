f1929b9f57ca29d16036bd1403b384abd06b709c
Daniel Kats <boompigdev@gmail.com>
2013-07-11 11:06:53 -0400

    Added CodeIgniter

---
+++ application/controllers/helloworld.php
@@ -0,0 +1,13 @@
+<?php
+	class Helloworld extends CI_Controller {
+		function index() {
+			$this->load->model('helloworld_model');
+
+			$data['result'] = $this->helloworld_model->getData();
+			$data['page_title'] = "CI Hello World App!";
+
+			$this->load->view('helloworld_view' , $data);
+		}
+	}
+
+?>
