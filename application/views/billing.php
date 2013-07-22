501e99c13723f47aa662b0e2424575c587727b69
Daniel Kats <boompigdev@gmail.com>
2013-07-22 12:43:15 -0400

    Improved look and feel of billing page, added client-side validation

--- application/views/billing.php
+++ application/views/billing.php
@@ -19,55 +19,183 @@ foreach (array("fName", "lName", "ccNum", "expMonth", "expYear") as $k) {
 		<title>Billing | Air U of T</title>
 		<meta charset="UTF-8" />
 		
+		<!-- Google-hosted JQuery -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		
+		<!-- Google-hosted JQuery UI -->
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+		
+		<!-- JQuery UI theme -->
+		<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/redmond/jquery-ui.css" />
+		
+		<!-- custom style -->
+		<link rel="stylesheet" href="<?=base_url(); ?>/css/style.css" />
+		<link rel="stylesheet" href="<?=base_url(); ?>/css/billing.css" />
+		
+		<!-- custom scripts -->
+		<script src="<?=base_url(); ?>/js/utils.js"></script>
+		
+		<script>
+			/**
+			 * Fill out all fields with junk data that will pass the validator.
+			 */
+			function junkFill () {
+				"use strict";
+				
+				var randDate = Date.randomFutureDate();
+				var randMonth = randDate.getMonth() + 1;
+				
+				$("#expMonth").val(String(randMonth).pad(2, "0"));
+				$("#expYear").val(randDate.getFullYear() - 2000);
+				
+				$("#ccNum").val(Utils.genRandomCC());
+				
+				var name = Utils.getRandomName().split(" ");
+				$("#fName").val(name[0]);
+				$("#lName").val(name[1]);
+			}
+		
+			/**
+			 * Return True iff the month and year are valid.
+			 * Invalid situations:
+			 * 	1. Month is invalid #
+			 * 	2. Credit card has expired
+			 * 
+			 * This function shows an error message if there is an error
+			 * Will not show the error if it looks like input is only partial
+			 */
+			function checkDate() {
+				var month = $("#expMonth").val();
+				var year = $("#expYear").val();
+				
+				$("#formError").hide();
+				
+				if (isNaN(month) || month.length == 0 || month < 1 || month > 12 || isNaN(year) || year.length == 0) {
+					return false;
+				} else {
+					// recall that JS numbers months from 0-11, so the first day of the month of expiry is (year, month - 1, 1)
+					// however, we want the first day of the month *after* expiry, = (year, (month - 1) + 1, 1)
+					var expiryDate = new Date("20" + String(year), Number(month), 1);
+					var now = new Date();
+					
+					// console.log(expiryDate);
+					// console.log(now);
+					// console.log("valid date? " + now < expiryDate);
+					
+					if (now >= expiryDate) {
+						$("#formError").show();
+					}
+					
+					return now < expiryDate;
+				}
+			}
+		
+			$(function() {
+				$("button, input[type=submit]").button();
+				
+				$(".ccExp, #ccNum").attr("type", "number");
+				
+				$("#myForm").submit(function () {
+					return checkDate();
+				});
+				
+				$("#expYear").blur(function() {
+					checkDate();
+				});
+				
+				$("#expYear").change(function() {
+					checkDate();
+				});
+				
+				$("#expMonth").keyup(function() {
+					if ($(this).val().length == 2) {
+						$("#expYear").focus();
+					}
+				});
+				
+				$("#autofill").click(function() {
+					junkFill();
+				});
+			});
+		</script>
 	</head>
 	
 	<body>
 		<h1>Billing</h1>
 		
-		<?=form_open("airuoft/confirm"); ?>
-			
-			<div>
+		<div id="container">
+			<?=form_open("airuoft/confirm", array("id" => "myForm")); ?>
+				
+				<fieldset class="userInput" id="nameContainer">
+					<?php
+						echo form_label("First Name");
+						$arr = HTML_Utils::get_input_array("fName");
+						$arr['required'] = 'required';
+						$arr['value'] = $_SESSION['fName'];
+						$arr['class'] = "name";
+						echo form_input($arr);
+						
+						echo form_label("Last Name");
+						$arr = HTML_Utils::get_input_array("lName");
+						$arr['required'] = 'required';
+						$arr['value'] = $_SESSION['lName'];
+						$arr['class'] = "name";
+						echo form_input($arr);
+					?>
+				</fieldset> <!-- end nameContainer -->
+				<fieldset class="userInput" id="ccContainer">
+					<legend>Credit Card Info</legend>
+					<?php
+						echo form_label("Credit Card Number");
+						$arr = HTML_Utils::get_input_array("ccNum");
+						$arr['required'] = 'required';
+						$arr['value'] = $_SESSION['ccNum'];
+						$arr['pattern'] = "\d{16}";
+						$arr['size'] = 16;
+						$arr['maxlength'] = 16;
+						$arr['oninvalid'] = "setCustomValidity('Credit card number must be 16 digits long with no dashes or spaces')";
+						echo form_input($arr);
+						
+						echo form_label("Expiry Date");
+						$arr = HTML_Utils::get_input_array("expMonth");
+						$arr['required'] = 'required';
+						$arr['value'] = $_SESSION['expMonth'];
+						$arr['size'] = 2;
+						$arr['maxlength'] = 2;
+						$arr['min'] = 1;
+						$arr['max'] = 12;
+						$arr['pattern'] = "\d{2}";
+						$arr['class'] = 'ccExp';
+						$arr['placeholder'] = "mm";
+						echo form_input($arr);
+						
+						echo "<span>/</span>";
+						
+						$arr = HTML_Utils::get_input_array("expYear");
+						$arr['required'] = 'required';
+						$arr['value'] = $_SESSION['expYear'];
+						$arr['size'] = 2;
+						$arr['maxlength'] = 2;
+						$arr['pattern'] = "\d{2}";
+						$arr['class'] = 'ccExp';
+						$arr['placeholder'] = "yy";
+						echo form_input($arr);
+					?>
+				</fieldset> <!-- end ccContainer -->
 				<?php
-					echo form_label("First Name: ");
-					$arr = HTML_Utils::get_input_array("fName");
-					$arr['required'] = 'required';
-					$arr['value'] = $_SESSION['fName'];
-					echo form_input($arr);
-					
-					echo form_label("Last Name: ");
-					$arr = HTML_Utils::get_input_array("lName");
-					$arr['required'] = 'required';
-					$arr['value'] = $_SESSION['lName'];
-					echo form_input($arr);
-					
-					// TODO have a pattern for this
-					echo form_label("Credit Card Number: ");
-					$arr = HTML_Utils::get_input_array("ccNum");
-					$arr['required'] = 'required';
-					$arr['value'] = $_SESSION['ccNum'];
-					echo form_input($arr);
-					
-					// TODO have a pattern for this
-					echo form_label("Credit Card Expiry: ");
-					$arr = HTML_Utils::get_input_array("expMonth");
-					$arr['required'] = 'required';
-					$arr['value'] = $_SESSION['expMonth'];
-					$arr['size'] = 2;
-					echo form_input($arr);
-					
-					echo "<span>/</span>";
-					
-					$arr = HTML_Utils::get_input_array("expYear");
-					$arr['required'] = 'required';
-					$arr['value'] = $_SESSION['expYear'];
-					$arr['size'] = 2;
-					echo form_input($arr);
-					
 					echo form_submit("submit", "Next");
 				?>
-			</div>
-			
-			
-			<?=form_close(); ?>
+				
+				<span id="autofill" class="ui-icon-pencil ui-icon"></span>
+				
+				<?=form_close(); ?>
+				
+				<div id="formError" class="ui-state-highlight ui-corner-all" style="display: none;">
+					<p>
+						<span class="ui-icon ui-icon-info"></span>
+						<span>Whoops! It looks like your credit card has expired!</span>
+					</p>
+				</div>
+		</div> <!-- end container -->
 	</body>
 </html>
--- js/utils.js
+++ js/utils.js
@@ -13,6 +13,22 @@ String.prototype.format = function () {
 };
 
 /**
+ * If a is specified, return a random number in the range [a, b].
+ * If b is not specified, return a random number in the range [0, a].
+ * 
+ * @param {Number} a
+ * @param {Number} b
+ */
+Math.randInt = function (a, b) {
+	if (b === undefined) {
+		b = a;
+		a = 0;
+	}
+	
+	return Math.floor(Math.random() * (b - a + 1)) + a;
+};
+
+/**
  * Wrapper for utility methods.
  */
 function Utils () {
@@ -40,4 +56,74 @@ Utils.isValidDate = function (dateStr) {
 	}
 	
 	return !isNaN(d.getTime());
-}
+};
+
+/**
+ * Generate a random credit-card number.
+ * i.e. a random 16-digit number
+ * 
+ * @returns {Number}
+ */
+Utils.genRandomCC = function () {
+	var n = 0, min;
+	
+	for (var i = 0; i < 16; i++) {
+		// ensures that leading digit will not be 0 (so makes sure we have enough digits)
+		min = i == 15 ? 1 : 0;
+		
+		n += Math.randInt(min, 9) * Math.pow(10, i);
+	}
+	
+	return n;
+};
+
+/**
+ * Return a random element from given array.
+ * @param {Array} arr
+ */
+Utils.randomChoice = function (arr) {
+	return arr[Math.randInt(0, arr.length - 1)];
+};
+
+Utils.getRandomName = function () {
+	var firstNames = ["Alice", "Will", "Ross", "Elia", "Sergei", "Jackie", "Andrew", "Kevin", "Daniel", "Sasha"];
+	var lastNames = ["Smith", "Liu", "Wu", "Slavovich", "Lee", "Jackson", "Snow", "Fierce"];
+	
+	return Utils.randomChoice(firstNames) + " " + Utils.randomChoice(lastNames);
+};
+
+/**
+ * Return a random date in the near future.
+ * Not uniformly random, but good enough for what I'm doing
+ * 
+ * @returns {Date}
+ */
+Date.randomFutureDate = function () {
+	// get some kind of random interval in the next 2 years
+	var dYear = Math.randInt(0, 1);
+	var dMonth = Math.randInt(0, 11);
+	var dDay = Math.randInt(1, 28);
+	var today = new Date();
+	
+	return new Date(today.getFullYear() + dYear, today.getMonth() + dMonth, today.getDate() + dDay);
+};
+
+/**
+ * Return a new string which is right-padded to `num` characters.
+ * Default character is "0"
+ * 
+ * @param {Number} num
+ * @param {String} c
+ */
+String.prototype.pad = function (num, c) {
+	if (c === undefined) {
+		c = "0";
+	}
+	
+	var a = Array(num - this.length);
+	for (var i = 0; i < num - this.length; i++) {
+		a[i] = c;
+	}
+	
+	return a.join("") + this;
+};
